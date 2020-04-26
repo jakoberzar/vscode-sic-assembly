09ab71b50e512367e59387811b038bc06e39a11f
Jakob Erzar <jakob.erzar@gmail.com>
2020-04-26 13:54:38 +0200

    Add document symbol provider.

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,12 @@ import { ExtensionContext, languages } from 'vscode';
 import { documentFormatter } from './document-formatter';
 import { SicCompletionItemProvider } from './completion-provider';
 import { SicDefinitionProvider } from './definition-provider';
+import { SicDocumentSymbolProvider } from './document-symbol-provider';
 
 export function activate(context: ExtensionContext) {
     const providerFormatting = languages.registerDocumentFormattingEditProvider('sic', documentFormatter);
     const providerCompletion = languages.registerCompletionItemProvider('sic', new SicCompletionItemProvider(), '\t ');
     const providerDefinition = languages.registerDefinitionProvider('sic', new SicDefinitionProvider());
-    context.subscriptions.push(providerFormatting, providerCompletion, providerDefinition);
+    const providerDocumentSymbols = languages.registerDocumentSymbolProvider('sic', new SicDocumentSymbolProvider());
+    context.subscriptions.push(providerFormatting, providerCompletion, providerDefinition, providerDocumentSymbols);
 }
